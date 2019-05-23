e99077f2d91d208a4e4b6f98d99b4aa4b56ab26a
Object-Pascal <passiboy@live.nl>
2019-05-23 12:19:00 +0200

    Startup scherm & Drawer stijl + basis functionaliteit

--- app/src/main/java/com/example/schatrijk_app/DashboardActivity.java
+++ app/src/main/java/com/example/schatrijk_app/DashboardActivity.java
@@ -1,9 +1,6 @@
 package com.example.schatrijk_app;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -13,7 +10,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity
+public class DashboardActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
---
+++ app/src/main/java/com/example/schatrijk_app/StartupSchermActivity.java
@@ -0,0 +1,34 @@
+package com.example.schatrijk_app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatImageButton;
+import android.view.View;
+import android.widget.Button;
+
+public class StartupSchermActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.startup_screen_main);
+        getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
+
+        RegisterInteractionEvents();
+    }
+
+    private void RegisterInteractionEvents() {
+        AppCompatImageButton btnParticipate = findViewById(R.id.btnparticipate);
+        btnParticipate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO: Check if the sensors exist and are in working order
+                if (true) {
+                    Intent i = new Intent(getApplicationContext(), new DashboardActivity().getClass());
+                    startActivity(i);
+                }
+            }
+        });
+    }
+}
