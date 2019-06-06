47a6ba9e45d26ace85bfabbe1e01aef07c16a204
Stijn <stijn.verhelpen@live.nl>
2019-06-06 14:05:22 +0200

    rebase

--- app/src/main/java/com/example/schatrijk_app/DashboardActivity.java
+++ app/src/main/java/com/example/schatrijk_app/DashboardActivity.java
@@ -21,6 +21,7 @@ public class DashboardActivity extends AppCompatActivity
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //NEED A COMMIT
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
