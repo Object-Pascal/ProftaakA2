1b80f890a00a449a0121eaa6fad6fa58fd1bbfe3
Object Pascal <passiboy@live.nl>
2019-06-06 12:41:36 +0200

    Dashboard addition & app icon logo

--- app/src/main/java/com/example/schatrijk_app/DashboardFragment.java
+++ app/src/main/java/com/example/schatrijk_app/DashboardFragment.java
@@ -1,11 +1,16 @@
 package com.example.schatrijk_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.AppCompatImageButton;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class DashboardFragment extends Fragment {
@@ -19,8 +24,29 @@ public class DashboardFragment extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         getActivity().setTitle("Dashboard");
+        RegisterInteractionEvents();
 
         TextView txtMessage = getActivity().findViewById(R.id.txtmessage);
-        txtMessage.setText("Geen schatten in de buurt!");
+
+        if (false) {
+            txtMessage.setText("Geen schatten in de buurt!");
+            AppCompatImageButton btnStart = getActivity().findViewById(R.id.btnstart);
+            btnStart.setVisibility(View.GONE);
+        }
+        else {
+            txtMessage.setText("Er is een schat opgedoken!");
+            AppCompatImageButton btnStart = getActivity().findViewById(R.id.btnstart);
+            btnStart.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void RegisterInteractionEvents() {
+        AppCompatImageButton btnStart = getActivity().findViewById(R.id.btnstart);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
