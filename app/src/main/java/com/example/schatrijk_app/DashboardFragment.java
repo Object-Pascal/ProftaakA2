237820e2e66c5375634cf49fdf5df2844f56f2c8
Hanno Brandwijk <brandwijkh@gmail.com>
2019-06-06 13:38:19 +0200

    Gradle + QR implementation

--- app/src/main/java/com/example/schatrijk_app/DashboardFragment.java
+++ app/src/main/java/com/example/schatrijk_app/DashboardFragment.java
@@ -1,13 +1,21 @@
 package com.example.schatrijk_app;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class DashboardFragment extends Fragment {
+
+    private Button button;
+    private TextView resultTV;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -18,5 +26,22 @@ public class DashboardFragment extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         getActivity().setTitle("Dashboard");
+
+        resultTV = getView().findViewById(R.id.resultTV);
+        button = getView().findViewById(R.id.qrcodebutton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openQRCode();
+            }
+        });
+
+    }
+
+    public void openQRCode (){
+        Intent intent = new Intent(getActivity().getApplicationContext(), QRCode.class);
+        intent.putExtra("KEY",345);
+        startActivity(intent);
     }
 }
